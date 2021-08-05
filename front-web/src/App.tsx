8033772118ba02a0ec6d9e1e924ed92bdb834fd7
Robson <robsong200@gmail.com>
2021-08-04 21:10:37 -0300

    Adicionando a Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Projeto criado</h1>
+    <>
+      <Navbar />
+      <h1>Projeto criado</h1>
+    </>
   );
 }
 
---
+++ front-web/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import './styles.scss';
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar navbar-expand-mb bg-warning main-nav">
+      <div className="container-fluid">
+        <a href="nav" className="nav-logo-text">
+          <h2>MovieFlix</h2>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
