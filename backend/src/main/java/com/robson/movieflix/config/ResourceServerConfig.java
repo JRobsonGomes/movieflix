31380ce06d9224ec679846a075a66eebc38fbadb
Robson <robsong200@gmail.com>
2021-08-11 22:39:02 -0300

    Redirect apos login, salvando sessao, ajustes CORS

--- backend/src/main/java/com/robson/movieflix/config/ResourceServerConfig.java
+++ backend/src/main/java/com/robson/movieflix/config/ResourceServerConfig.java
@@ -56,11 +56,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration corsConfig = new CorsConfiguration();
+		CorsConfiguration corsConfig = new CorsConfiguration().applyPermitDefaultValues();
 		corsConfig.setAllowedOrigins(Arrays.asList("*"));
 		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
-		corsConfig.setAllowCredentials(true);
-		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+//		corsConfig.setAllowCredentials(true);
+//		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfig);
--- front-web/src/components/Login/index.tsx
+++ front-web/src/components/Login/index.tsx
@@ -1,17 +1,57 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory, useLocation } from "react-router-dom";
+import { saveSessionData } from "utils/auth";
+import { makeLogin } from "utils/request";
 import './styles.scss';
 
+type FormState = {
+  username: string;
+  password: string;
+};
+
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormState>();
+  const [hasError, setHasError] = useState(false);
+  const history = useHistory();
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: "/movies" } };
+
+  const onSubmit = (data: FormState) => {
+    console.log(data);
+    makeLogin(data)
+      .then(response => {
+        setHasError(false);
+        saveSessionData(response.data);
+        history.replace(from);
+      })
+      .catch(() => {
+        setHasError(true);
+      });
+  }
 
   return (
     <div className="login-container card-base">
       <h1>Login</h1>
-      <form>
+      {hasError && (
+        <div className="alert alert-danger p-2 mt-3" role="alert">
+          Usuário ou senha inválidos!
+        </div>)
+      }
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          {...register("username", { required: true })}
           className="form-control"
           type='text'
           placeholder='Email'
         />
         <input
+          {...register("password", { required: true })}
           className="form-control"
           type='password'
           placeholder='Senha'
