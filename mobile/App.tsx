c3e830b51f02ed18034d6d08f14737743c090e26
Robson <robsong200@gmail.com>
2021-09-11 14:09:24 -0300

    Projeto mobile criado

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>Home</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#525252',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  h1: {
+    fontWeight: '500',
+    fontSize: 24,
+    color: '#fff'
+  }
+});
